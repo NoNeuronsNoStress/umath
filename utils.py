a393f9b0d1030e632fce6dea5e45d8694e570e18
NoNeuronsNoStress <101355029+NoNeuronsNoStress@users.noreply.github.com>
2024-10-21 21:58:07 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 import math
+def filter_f(s): 
+    if int(str(s)[-1]) in [1,3,7,9]:
+        return True  
 def slieve(n):
-    number_list = [k for k in range(n+1)][2:] 
+    if n <= 10: 
+        number_list = [k for k in range(n+1)][2:]
+    else:
+        
+        number_list = [2,3,5,7] + [k for k in range(10,n+1,1) if filter_f(k) == True]
     for i in range(math.floor(n**0.5)):
         multiplier = 1
         r = number_list[i]*multiplier 
