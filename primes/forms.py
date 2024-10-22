3c960cfa2dbcc174510cf8c7b7a333ac9b4ee699
NoNeuronsNoStress <101355029+NoNeuronsNoStress@users.noreply.github.com>
2024-10-22 19:18:45 +0200

    Add files via upload

--- primes/forms.py
+++ primes/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 
-class InputUpperLimit(forms.Form): 
-    upper_limit = forms.CharField(label="limit", max_length=25)
-
+class InputUpperLimit(forms.Form):
+    upper_limit = forms.CharField(label="",max_length=25, widget=forms.TextInput(attrs={
+        'style': 'text-align: center; margin: 0 auto; display: block; position: absolute; top: 47%; left: 50%; transform: translate(-50%, -50%);',
+        'class': 'input-upper-limit'
+    }))
